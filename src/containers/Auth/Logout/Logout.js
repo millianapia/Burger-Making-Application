455ad8b8cd75857301618a243c1f8af9f8c9cf1a
Malin J <millianapia@gmail.com>
2021-03-15 12:49:50 +0100

    Was buggy, is now not buggy

--- src/containers/Auth/Logout/Logout.js
+++ src/containers/Auth/Logout/Logout.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
- 
+
 import * as actions from '../../../store/actions/index';
 
 const logout = props => {
